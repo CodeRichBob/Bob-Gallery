b0312c8600c4d3cd888fab9b9a9a37ce1bedf900
Robert <rgitonga.07@gmail.com>
2022-05-30 01:32:29 +0300

    install postgress dependency

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def gallery(request):
+    location = request.GET.get('location')
+
+    if location ==  None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(location__name = location)
+
+
+
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+
+    return render(request,'photos/gallery.html', {"categories" : categories,"photos" : photos,"locations" : locations})
