9466eb5240449b0e90a256b9aa7de6cdd355a245
Robert <rgitonga.07@gmail.com>
2022-05-30 01:44:18 +0300

    ft: add views functions

--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Category,Photo,Location
 
 # Create your views here.
 
@@ -16,3 +17,22 @@ def gallery(request):
     locations = Location.objects.all()
 
     return render(request,'photos/gallery.html', {"categories" : categories,"photos" : photos,"locations" : locations})
+
+def viewPhoto(request,pk):
+    photo = Photo.objects.get(id = pk)
+
+
+    return render(request,'photos/photo.html', {"photo" : photo})
+    
+def search_results(request):
+
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_articles = Photo.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'photos/search.html',{"message":message,"categories": searched_articles})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'photos/search.html',{"message":message}) 
